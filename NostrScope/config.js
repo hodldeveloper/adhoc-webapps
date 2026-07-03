48b58f167cddbe1a770626ad26d87c064b52e66b
hodldeveloper <hodlcashtoken@gmail.com>
2026-07-03 13:13:48 +0800

    Update config.js

--- NostrScope/config.js
+++ NostrScope/config.js
@@ -1,6 +1,5 @@
 // NostrScope configuration
 const CONFIG = {
-    // Relay pool – edit this array to change the relays used by the app
     relays: [
         'wss://relay.damus.io',
         'wss://nos.lol',
@@ -12,14 +11,8 @@ const CONFIG = {
         'wss://relay.ditto.pub',
         'wss://relay.bchnostr.com'
     ],
-
-    // Event investigation – stop searching after this many events found
-    earlyStopThreshold: 10,
-
-    // Maximum depth for thread investigation
-    maxDepth: 4,
-
-    // Timeouts (ms)
+    earlyStopThreshold: 10,          // stop event investigation after this many events
+    maxDepth: 4,                     // max thread depth
     relayConnectTimeout: 8000,
     profileInvestigationTimeout: 10000,
     quickProfileTimeout: 3000,
